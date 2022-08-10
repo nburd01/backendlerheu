c318f7db44c43797e34d9cd182d7d809f61c2dc5
Niall Burdon <n.burdon@protonmail.com>
2022-08-10 16:50:10 +0200

    added devise_parameter_sanitizer to app controller to allow user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :first_name, :second_name) }
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :first_name, :second_name)}
+  end
+
 end
