81d046067da9120c5208d6dd99e39275afd4938d
Niall Burdon <n.burdon@protonmail.com>
2022-08-11 12:40:32 +0200

    added params to scores in order to update

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -46,6 +46,6 @@ class ScoresController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def score_params
-      params.fetch(:score, {})
+      params.require(:score).permit(:locaux, :score_locaux, :visiteurs, :score_visiteurs, :division, :equipe, :categorie)
     end
 end
