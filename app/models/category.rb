ff3fd7f44eb10e9d970717a4d80a4e6cc15638e0
Niall Burdon <n.burdon@protonmail.com>
2022-08-29 15:15:48 +0200

    added relationships and cleaned up until competitions

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
     belongs_to :discipline
     has_many :players
     has_many :teams
+    has_many :competitions, through: :teams
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,14 @@
 require 'database_cleaner/active_record'
 DatabaseCleaner.clean_with(:truncation)
 
-Discipline.destroy_all
 User.destroy_all
+Discipline.destroy_all
 Category.destroy_all
-Player.destroy_all
-Score.destroy_all
 Team.destroy_all
+Player.destroy_all
+Competition.destroy_all
+Score.destroy_all
+Match.destroy_all
 
 
 
@@ -34,15 +36,15 @@ end
 
 3.times do |r|
 
-    score = Score.create!(
-          locaux: "Locaux",
-          score_locaux: rand(1..3),
-          visiteurs: "Le Rheu",
-          score_visiteurs:rand(1..3),
-          division: "division" ,
-          equipe: "equipe" ,
-          categorie:  "categorie"
-    );
+score = Score.create!(
+      locaux: "Locaux",
+      score_locaux: rand(1..3),
+      visiteurs: "Le Rheu",
+      score_visiteurs:rand(1..3),
+      division: "division" ,
+      equipe: "equipe" ,
+      categorie:  "categorie"
+);
   
 end
 
@@ -54,82 +56,118 @@ user = User.create!(
 
 
 Discipline.create(
-          name: 'football', 
-          discipline_img:'https://s3.static-footeo.com/1200/uploads/sclerheu/gallery/72709234_2411099205648659_1072033552297623552_o__pz6mhj.jpg',
-          discipline_root:'football')
+  name: 'football', 
+  discipline_img:'https://s3.static-footeo.com/1200/uploads/sclerheu/gallery/72709234_2411099205648659_1072033552297623552_o__pz6mhj.jpg',
+  discipline_root:'football'
+          )
 Discipline.create(
-          name: 'beach soccer', 
-          discipline_img:'https://fff.twic.pics/https://media.fff.fr/uploads/images/4d7125038d960b502bef5aa2bdc9021a.png?twic=v1/resize=730',
-          discipline_root:'beach')
+  name: 'beach soccer', 
+  discipline_img:'https://fff.twic.pics/https://media.fff.fr/uploads/images/4d7125038d960b502bef5aa2bdc9021a.png?twic=v1/resize=730',
+  discipline_root:'beach'
+          )
 Discipline.create(
-          name: 'futsal', 
-          discipline_img:'https://fff.twic.pics/https://media.fff.fr/uploads/images/ecd02c5415985e752f4aad7c3b61086d.png?twic=v1/resize=730',
-          discipline_root:'futsal')
+  name: 'futsal', 
+  discipline_img:'https://fff.twic.pics/https://media.fff.fr/uploads/images/ecd02c5415985e752f4aad7c3b61086d.png?twic=v1/resize=730',
+  discipline_root:'futsal'
+          )
 Discipline.create(
-          name: 'fitfoot', 
-          discipline_img:'https://lfhf.fff.fr/wp-content/uploads/sites/15/2019/12/FitFoot-fille-611x378.png',
-          discipline_root:'fitfoot')
+  name: 'fitfoot', 
+  discipline_img:'https://lfhf.fff.fr/wp-content/uploads/sites/15/2019/12/FitFoot-fille-611x378.png',
+  discipline_root:'fitfoot'
+          )
 Discipline.create(
-          name: 'féminines', 
-          discipline_img:'https://imgs.search.brave.com/sitYDMmQPE4PfL4UN-fseK8l4VLQ2TfgrGas5O8TIu8/rs:fit:639:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/V0UtaHlHNFVkdHhY/c2hkdk5oUW9nQUFB/QSZwaWQ9QXBp',
-          discipline_root:'feminines')
+  name: 'féminines', 
+  discipline_img:'https://imgs.search.brave.com/sitYDMmQPE4PfL4UN-fseK8l4VLQ2TfgrGas5O8TIu8/rs:fit:639:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/V0UtaHlHNFVkdHhY/c2hkdk5oUW9nQUFB/QSZwaWQ9QXBp',
+  discipline_root:'feminines'
+          )
 Discipline.create(
-          name: 'arbitrage', 
-          discipline_img:'https://imgs.search.brave.com/urKZiDMJSGPgW8IfRrWlQpF68aoZ-YmoQG6naYDVSgU/rs:fit:766:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/aWlBSmpmaVA0czRW/a1gyVENDVlNRSGFF/bCZwaWQ9QXBp',
-          discipline_root:'arbitrage')
+  name: 'arbitrage', 
+  discipline_img:'https://imgs.search.brave.com/urKZiDMJSGPgW8IfRrWlQpF68aoZ-YmoQG6naYDVSgU/rs:fit:766:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/aWlBSmpmaVA0czRW/a1gyVENDVlNRSGFF/bCZwaWQ9QXBp',
+  discipline_root:'arbitrage'
+          )
 
 
  Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://s3.static-footeo.com/1200/uploads/sclerheu/gallery/72709234_2411099205648659_1072033552297623552_o__pz6mhj.jpg'
+    title: Faker::Book.title, 
+    postBg:'https://s3.static-footeo.com/1200/uploads/sclerheu/gallery/72709234_2411099205648659_1072033552297623552_o__pz6mhj.jpg'
             )
   Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://fff.twic.pics/https://media.fff.fr/uploads/images/4d7125038d960b502bef5aa2bdc9021a.png?twic=v1/resize=730'
+    title: Faker::Book.title, 
+    postBg:'https://fff.twic.pics/https://media.fff.fr/uploads/images/4d7125038d960b502bef5aa2bdc9021a.png?twic=v1/resize=730'
             )
   Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://fff.twic.pics/https://media.fff.fr/uploads/images/ecd02c5415985e752f4aad7c3b61086d.png?twic=v1/resize=730'
+    title: Faker::Book.title, 
+    postBg:'https://fff.twic.pics/https://media.fff.fr/uploads/images/ecd02c5415985e752f4aad7c3b61086d.png?twic=v1/resize=730'
             )
   Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://lfhf.fff.fr/wp-content/uploads/sites/15/2019/12/FitFoot-fille-611x378.png'
+    title: Faker::Book.title, 
+    postBg:'https://lfhf.fff.fr/wp-content/uploads/sites/15/2019/12/FitFoot-fille-611x378.png'
             )
   Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://imgs.search.brave.com/sitYDMmQPE4PfL4UN-fseK8l4VLQ2TfgrGas5O8TIu8/rs:fit:639:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/V0UtaHlHNFVkdHhY/c2hkdk5oUW9nQUFB/QSZwaWQ9QXBp'
+    title: Faker::Book.title, 
+    postBg:'https://imgs.search.brave.com/sitYDMmQPE4PfL4UN-fseK8l4VLQ2TfgrGas5O8TIu8/rs:fit:639:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/V0UtaHlHNFVkdHhY/c2hkdk5oUW9nQUFB/QSZwaWQ9QXBp'
             )
   Post.create(
-            title: Faker::Book.title, 
-            postBg:'https://imgs.search.brave.com/urKZiDMJSGPgW8IfRrWlQpF68aoZ-YmoQG6naYDVSgU/rs:fit:766:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/aWlBSmpmaVA0czRW/a1gyVENDVlNRSGFF/bCZwaWQ9QXBp'
+    title: Faker::Book.title, 
+    postBg:'https://imgs.search.brave.com/urKZiDMJSGPgW8IfRrWlQpF68aoZ-YmoQG6naYDVSgU/rs:fit:766:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5S/aWlBSmpmaVA0czRW/a1gyVENDVlNRSGFF/bCZwaWQ9QXBp'
             )
 
-  3.times do |r|
-
-    categorie = Category.create!(
-      name: "categorie #{r}",
-      description: "description #{r}",
-      discipline_id: rand(1..6)
-    );
-  
-  end
+  Category.create(
+    name: "seniors",
+    discipline_id: 1
+            )
+  Category.create(
+    name: "academy",
+    discipline_id: 2
+            )
+  Category.create(
+    name: "école de foot",
+    discipline_id: 3
+            )
+  Category.create(
+    name: "vétérans",
+    discipline_id: 4
+            )
 
 
   Player.create(
-            first_name: Faker::Movies::StarWars.character,
-            player_img: 'https://www.picclickimg.com/d/l400/pict/373395498468_/237-Dimitri-Payet-%E2%9A%BD-Olympique-Marseille-Om-Sticker.jpg',
-            category_id: 1
+    first_name: Faker::Movies::StarWars.character,
+    player_img: 'https://www.picclickimg.com/d/l400/pict/373395498468_/237-Dimitri-Payet-%E2%9A%BD-Olympique-Marseille-Om-Sticker.jpg',
+    category_id: 1
             )
   Team.create(
-            name: 'Team 1',
-            category_id: 1
+    name: 1,
+    category_id: 1
+            )
+  Team.create(
+    name: 2,
+    category_id: 2
+            )
+  Team.create(
+    name: 3,
+    category_id: 3
             )
 
-3.times do |r|
+  Competition.create(
+    name: "R2",
+    team_id: 1,
+    category_id:1
+            )
+  Competition.create(
+    name: "R3",
+    team_id: 2,
+    category_id:1
+            )
+  Competition.create(
+    name: "D2",
+    team_id: 3,
+    category_id:1
+            )
 
-  team = Team.create!(
-    name: "Team #{r}",
-    category_id: 1
-  );
-
-end
+  Match.create(
+    content: "D2",
+    venue_id:3,
+    calendar_id:1,
+    opponent_id:1,
+    competition_id:1,
+            )
